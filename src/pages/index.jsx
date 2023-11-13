be268c8a75d307a281fe218e029c4df23ef84235
eehd80 <eehd80yul@gmail.com>
2023-11-13 12:26:46 +0900

    home set

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { SimpleGrid, Box } from '@chakra-ui/react';
 import Layout from '../components/layout/Layout';
 
 const Home = () => {
-    return <Layout title="DashBoard" pagename="DashBoard"></Layout>;
+    return (
+        <Layout title="DashBoard" pagename="DashBoard">
+            <SimpleGrid columns={[1, 2, 3, null, 6]} spacing={10}>
+                <Box bg="tomato" height="80px"></Box>
+                <Box bg="tomato" height="80px"></Box>
+                <Box bg="tomato" height="80px"></Box>
+                <Box bg="tomato" height="80px"></Box>
+                <Box bg="tomato" height="80px"></Box>
+                <Box bg="tomato" height="80px"></Box>
+            </SimpleGrid>
+        </Layout>
+    );
 };
 
 export default Home;
